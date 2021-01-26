88fbde446b728359bf1554d0002a3424cb66e446
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-01-26 13:47:17 +0300

    this changes for next version vue-teting

--- tests/unit/MessageContainer.spec.js
+++ tests/unit/MessageContainer.spec.js
@@ -3,9 +3,12 @@ import { mount } from '@vue/test-utils'
 
 describe('MessageContainer', () => {
   it('Wrap MessageDisplay Component', () => {
+    const MessageDisplayMock0 = {
+      template: '<p v-else data-testid="message">hello from DB</p>'
+    }
     const wrapper = mount(MessageContainer, {
       stubs: {
-        MessageDisplay: '<p v-else data-testid="message">hello from DB</p>'
+        MessageDisplay: MessageDisplayMock0
       }
     })
     const message = wrapper.find('[data-testid="message"]').element.textContent
